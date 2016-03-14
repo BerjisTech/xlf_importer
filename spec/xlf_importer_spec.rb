9c44c37b2888273bbf1ab79f0c29db1050611b88
Kevin Dias <diasks2@gmail.com>
2016-03-14 12:05:40 +0900

    Bump version

--- spec/xlf_importer_spec.rb
+++ spec/xlf_importer_spec.rb
@@ -62,28 +62,10 @@ describe XlfImporter do
       expect(xlf.import[1].length).to eq(5)
     end
 
-    it 'imports sample_alt_2.xlf' do
-      file_path = File.expand_path('../xlf_importer/spec/test_sample_files/sample_alt_2.xlf')
-      xlf = XlfImporter::Xlf.new(file_path: file_path, encoding: 'UTF-8')
-      expect(xlf.import).to eq('')
-    end
-
     it 'imports a UTF-16 XLIFF file' do
       file_path = File.expand_path('../xlf_importer/spec/test_sample_files/sample_utf-16.xlf')
       xlf = XlfImporter::Xlf.new(file_path: file_path)
       expect(xlf.import[1][0][4]).to eq("Hello world")
     end
-
-    # it 'imports a desktop5' do
-    #   file_path = File.expand_path('/Users/diasks2/Downloads/5.xlf')
-    #   xlf = XlfImporter::Xlf.new(file_path: file_path)
-    #   expect(xlf.import[1].length).to eq(1594)
-    # end
-
-    # it 'imports a desktop5' do
-    #   file_path = File.expand_path('/Users/diasks2/Downloads/5.xlf')
-    #   xlf = XlfImporter::Xlf.new(file_path: file_path)
-    #   expect(xlf.import[0].length).to eq(797)
-    # end
   end
 end
